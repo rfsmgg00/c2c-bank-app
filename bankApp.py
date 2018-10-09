01c35c2063235a5b4769acf0ee9785a8a92fca6b
Roberto <roberto.florentino.salinas@gmail.com>
2018-10-09 22:29:10 +0000

    Add conditionals

--- bankApp.py
+++ bankApp.py
@@ -8,9 +8,12 @@ login_types = ["admin", "user", "guest"]
 # For “guest”:
 # program says “You have no privileges.”
 def gatekeeper(login):
-  if login == "admin":
-    return "You have the privileges"
-
+    if login == "admin":
+        return "You have the privileges"
+    elif login == "user":
+        return "You have limited privileges."
+    elif login == "guest":
+        return "You have no privileges."
 # 3. Call the gatekeeper function with a string and print what it returns.
 print(gatekeeper("admin"))
 
@@ -19,7 +22,11 @@ print(gatekeeper("admin"))
 # 5. Complete the function called check_balance that takes one parameter, loan_balance.
 def check_balance(loan_balance):
 # 6. If loan_balance is zero or more, it says “you don’t owe any money”
+    if loan_balance >= 0:
+        return "You do not owe any money"
 # 7. If loan_balance is negative, it  says “you owe $X” where X is the amount
+    else:
+        return "You owe $" + str(loan_balance)
     return
 
 # 8. Call your function with a negative and positive number and print what it returns.
